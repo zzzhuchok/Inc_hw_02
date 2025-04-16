cacad5eaceebcd1aace1cef90fb9d30ede295af4
Rostislav Zhuk <zzzhuchok@gmail.com>
2025-04-16 22:11:33 +0300

    feat: add task s-3-2

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -12,49 +12,50 @@ import s from './HW6.module.css'
  */
 
 const HW6 = () => {
-    const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
 
-    const save = () => {
-        saveState<string>('hw6-editable-span-value', value)
-    }
-    const restore = () => {
-        // делают студенты
+  const save = () => {
+    saveState<string>('hw6-editable-span-value', value)
+  }
+  const restore = () => {
+    // делают студенты
+    const lsValue = restoreState<string>('hw6-editable-span-value', value)
+    setValue(lsValue)
+  }
 
-    }
+  return (
+    <div id={'hw6'}>
+      <div className={s2.hwTitle}>Homework #6</div>
 
-    return (
-        <div id={'hw6'}>
-            <div className={s2.hwTitle}>Homework #6</div>
-
-            {/*демонстрация возможностей компоненты:*/}
-            <div className={s2.hw}>
-                <div className={s.editableSpanContainer}>
-                    <SuperEditableSpan
-                        id={'hw6-spanable-input'}
-                        value={value}
-                        onChangeText={setValue}
-                        spanProps={{
-                            id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
-                        }}
-                    />
-                </div>
-
-                <div className={s.buttonsContainer}>
-                    <SuperButton id={'hw6-save'} onClick={save}>
-                        Save to ls
-                    </SuperButton>
-                    <SuperButton
-                        id={'hw6-restore'}
-                        onClick={restore}
-                        xType={'secondary'}
-                    >
-                        Get from ls
-                    </SuperButton>
-                </div>
-            </div>
+      {/*демонстрация возможностей компоненты:*/}
+      <div className={s2.hw}>
+        <div className={s.editableSpanContainer}>
+          <SuperEditableSpan
+            id={'hw6-spanable-input'}
+            value={value}
+            onChangeText={setValue}
+            spanProps={{
+              id: 'hw6-editable-span',
+              defaultText: 'enter text...',
+            }}
+          />
         </div>
-    )
+
+        <div className={s.buttonsContainer}>
+          <SuperButton id={'hw6-save'} onClick={save}>
+            Save to ls
+          </SuperButton>
+          <SuperButton
+            id={'hw6-restore'}
+            onClick={restore}
+            xType={'secondary'}
+          >
+            Get from ls
+          </SuperButton>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HW6
